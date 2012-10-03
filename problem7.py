30997aaad583286b18c4711ac1ae89f3f7a313c6
Peter Vel <petervel@gmail.com>
2012-10-03 21:56:18 +0200

    solved problem 7

---
+++ problem7.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python
+SIEVE_SIZE = 200000
+
+def create_list():
+	return range(0, SIEVE_SIZE)
+
+def remove_from_list(i, list):
+	index = 2 * i
+	while index < SIEVE_SIZE:
+		list[index] = 0
+		index += i
+
+def sieve():
+	list = create_list()
+	for i in list:
+		if i >= 2:
+			remove_from_list(i, list)
+	return list
+
+def main():
+	list = sieve()
+	index = 0
+	for i in list:
+		if i >= 2:
+			index += 1
+			print("{0}: {1}".format(index, i))
+
+main()
