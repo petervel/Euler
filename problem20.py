79a5c0ccb7bba58ab476181fa667452cef848766
petervel <peter.vel@nedap.com>
2012-10-06 22:33:10 +0200

    solved problem 20

--- problem20.py
+++ problem20.py
@@ -1,2 +1,19 @@
 #!/usr/bin/python
 __author__ = 'peter.vel'
+
+def sum_digits(number):
+	number = str(number)
+	sum = 0
+	for i in number:
+		sum += int(i)
+	return sum
+
+def fac(number):
+	if number <= 1:
+		return 1
+	return number * fac(number - 1)
+
+def main():
+	print(sum_digits(fac(100)))
+
+main()
