86c957faf081f1403bb2c79b3f98ef88b898737c
Peter Vel <peter.vel@nedap.com>
2012-10-07 23:01:50 +0200

    solved problem 45

--- problems_026-050/problem45.py
+++ problems_026-050/problem45.py
@@ -10,24 +10,42 @@ def pentagonal(n):
 def hexagonal(n):
 	return n * (2 * n - 1)
 
-def generate_series(method, count):
+def generate_series(method, max):
 	list = []
-	for i in range(100000, count):
-		list.append(method(i))
+	i = 0
+	while True:
+		value = method(i)
+		if value > max:
+			break
+
+		list.append(value)
+		i += 1
+		
+	print("last value was: {0}".format(list[-1]))
 	return list
+
+def ordered_list_contains(list, item):
+	for i in list:
+		if i > item:
+			return False
+		if i == item:
+			return True
+	return False
 
 def find_matches(list1, list2, list3):
 	list = []
-	for i in list1:
-		if i in list2 and i in list3:
+	for i in list3:
+		if ordered_list_contains(list2, i) and ordered_list_contains(list1, i):
 			list.append(i)
 	return list
 
 def main():
-	tris = generate_series(triangle, 500000)
-	pents = generate_series(pentagonal, 500000)
-	hexes = generate_series(hexagonal, 500000)
+	max = 2000000000
+	tris = generate_series(triangle, max)
+	pents = generate_series(pentagonal, max)
+	hexes = generate_series(hexagonal, max)
 	matches = find_matches(tris, pents, hexes)
+
 	for i in matches:
 		print(i)
 
