766dd0c5839e63de2c96f0c68b0d9e22b807dcda
petervel <peter.vel@nedap.com>
2012-10-07 17:33:06 +0200

    solved problem 31

---
+++ problems_026-050/problem31.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python
+__author__ = 'peter.vel'
+
+coins = [200, 100, 50, 20, 10, 5, 2, 1]
+
+def count_ways(cents, coin_index):
+	if cents < 0:
+		return 0
+
+	if cents == 0:
+		return 1
+
+	sum = 0
+
+	for i in range(coin_index, len(coins)):
+		if coins[i] <= cents:
+			sum += count_ways(cents - coins[i], i)
+
+	return sum
+
+def main():
+	print(count_ways(200, 0))
+
+main()
