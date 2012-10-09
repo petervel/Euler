9cdc34c7cc92cdf26fe06f54ffa4d2a941eab815
petervel <peter.vel@nedap.com>
2012-10-09 18:53:52 +0200

    FINALLY solved problem 23

--- problems_001-025/problem23.py
+++ problems_001-025/problem23.py
@@ -1,55 +1,28 @@
 #!/usr/bin/python
 __author__ = 'peter.vel'
 
-def list_contains(list, item):
-	for i in list:
-		if i == item:
-			return True
-	return False
-
-def get_divisors(number):
-	divisors = []
-	half = int((number / 2) + 1)
-	for i in range(1, half):
+def sum_divisors(number):
+	sum = 1
+	sqrt = number ** 0.5
+	last = int(sqrt) + 1
+	for i in range(2, last):
 		if number % i == 0:
-			divisors.append(i)
-	return divisors
-
-def is_abundant(number):
-	divisors = get_divisors(number)
-	sum = 0
-	for i in divisors:
-		sum += i
-	return sum > number
-
-def list_abundant_numbers(max):
-	list = []
-	for i in range(1, max):
-		if is_abundant(i):
-			list.append(i)
-	return list
-
-def is_sum_of_two_numbers(list, number):
-	for i in list:
-		if i >= number:
-			return False
-
-		other = number - i
-		if list_contains(list, other):
-			return True
-	return False
-
-def sum_numbers(list, numbers):
-	sum = 0
-	for i in numbers:
-		if not is_sum_of_two_numbers(list, i):
 			sum += i
+			sum += number / i
+
+	if sqrt == int(sqrt):
+		sum -= sqrt #we added it twice
+
 	return sum
 
 def main():
-	list = list_abundant_numbers(28123)
-	numbers = range(1, 28123)
-	sum = sum_numbers(list, numbers)
+	sum = 0
+	abundant_numbers = set()
+	for i in range(1, 28123):
+		if sum_divisors(i) > i:
+			abundant_numbers.add(i)
+		if not any( (i - a in abundant_numbers) for a in abundant_numbers):
+			sum += i
 	print(sum)
 
 main()
