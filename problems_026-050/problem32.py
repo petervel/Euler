775ae4eb9bb85b391fb8fcff65a581987e257c5c
petervel <peter.vel@nedap.com>
2012-10-11 11:55:03 +0200

    made python files executable

--- problems_026-050/problem32.py
+++ problems_026-050/problem32.py
@@ -1,12 +1,17 @@
 #!/usr/bin/python
 __author__ = 'peter.vel'
 
-def sum_pandigital_products(digits):
-	return 0
+products = []
+
+def sum_pandigital_products(digits, multiplicand):
+	for i in range(1, 10):
+		subset = list(digits)
+		subset.remove(str(i))
+	return sum(products)
 
 def main():
 	digits = ['1', '2', '3', '4', '5', '6', '7', '8', '9']
-	sum = sum_pandigital_products(digits)
+	sum = sum_pandigital_products(digits, 0)
 	print(sum)
 
 main()
