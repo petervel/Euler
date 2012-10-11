e3b7f00fb1d75694692e18c30cdd29c083fbee75
Peter Vel <peter.vel@nedap.com>
2012-10-11 22:19:41 +0200

    started problem 54

--- problems_051-075/problem54.py
+++ problems_051-075/problem54.py
@@ -1,6 +1,30 @@
 #!/usr/bin/python
 __author__ = 'peter.vel'
 
+class Hand:
+	def __init__(self, cards):
+		self.cards = cards
+
+	def __cmp__(self, other):
+		return 1
+
+class Round:
+	def __init__(self, line):
+		cards = line.split(" ")
+		self.mine = Hand(cards[:5])
+		self.his = Hand(cards[5:])
+
+	def winner(self):
+		return self.mine > self.his
+
+def read_rounds(file):
+	open_file = open(file)
+	lines = open_file.readlines()
+	return [Round(line) for line in lines]
+
 def main():
+	rounds = read_rounds("data/poker.txt")
+	count = sum(1 for round in rounds if round.winner())
+	print(count)
 
 main()
