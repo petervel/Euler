0c42fdc1b4e278f5c5eecdecb3275ebca06152da
petervel <peter.vel@nedap.com>
2012-10-13 15:53:12 +0200

    solved problem 32

--- problems_026-050/problem32.py
+++ problems_026-050/problem32.py
@@ -1,17 +1,51 @@
 #!/usr/bin/python
 __author__ = 'peter.vel'
 
-products = []
+all_digits = ['1','2','3','4','5','6','7','8','9']
 
-def sum_pandigital_products(digits, multiplicand):
-	for i in range(1, 10):
-		subset = list(digits)
-		subset.remove(str(i))
-	return sum(products)
+def generate_pandigitals(digits):
+	if len(digits) == 1:
+		return digits
+
+	pandigitals = []
+	for i in digits:
+		digits_without_i = list(digits)
+		digits_without_i.remove(i)
+		subset = generate_pandigitals(digits_without_i)
+		for sub in subset:
+			pandigitals.append(i + sub)
+	return pandigitals
+
+def check(number):
+	for i in range(1,5):
+		for j in range(i+1, 7):
+			multiplier = int(number[:i])
+			multiplicand = int(number[i:j])
+			product = int(number[j:])
+			if multiplier * multiplicand == product:
+				print("{0} x {1} = {2}".format(multiplier, multiplicand, product))
+				return product
+	return 0
+
+def filter_formulas(pandigitals):
+	products = []
+	for number in pandigitals:
+		product = check(number)
+		if product != 0:
+			products.append(product)
+	return products
+
+def remove_duplicates(list):
+	found = []
+	for i in list:
+		if i not in found:
+			found.append(i)
+	return found
 
 def main():
-	digits = ['1', '2', '3', '4', '5', '6', '7', '8', '9']
-	sum = sum_pandigital_products(digits, 0)
-	print(sum)
+	pandigitals = generate_pandigitals(all_digits)
+	working_values = filter_formulas(pandigitals)
+	unique = remove_duplicates(working_values)
+	print(sum(unique))
 
 main()
