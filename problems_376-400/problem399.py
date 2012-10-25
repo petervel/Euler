b1711d7eed36e219bcd441b77dba11f16190b68d
petervel <peter.vel@nedap.com>
2012-10-25 19:59:47 +0200

    All working except for the is_squarefree() method

--- problems_376-400/problem399.py
+++ problems_376-400/problem399.py
@@ -9,42 +9,26 @@ TAIL_MAX = 10 ** TAIL_SIZE
 
 SQUARES = [n*n for n in range(2, 100)]
 
-class ScientificNotation:
-	def __init__(self, head, exp):
+class HugeNumber:
+	def __init__(self, head, exp, tail):
 		self.head = head
 		self.exp = exp
-
-	def calc_head(self, other):
-		if other.exp < self.exp:
-			return 10 * self.head
-		else:
-			return self.head
-
-	def add(self, other):
-		self_head = self.calc_head(other)
-		other_head = other.calc_head(self)
-		head = self_head + other_head
-		exp = min(self.exp, other.exp)
-		while head > HEAD_MAX:
-			head /= 10
-			exp += 1
-
-		return ScientificNotation(head, exp)
-
-	def __str__(self):
-		return "{0}e{1}".format(self.head, self.exp)
-
-class HugeNumber:
-	def __init__(self, head, scientific_notation, tail):
-		self.head = head
-		self.scientific_notation = scientific_notation
 		self.tail = tail
 
 	def add(self, other):
-		scientific_notation = self.scientific_notation.add(other.scientific_notation)
+		converted_head = self.head
+		exp = other.exp #assume lowest exponent
+		if self.exp > other.exp:
+			converted_head *= 10 #convert potentially larger number to lower exp
+
+		head = converted_head + other.head
+		while head > 10:	#convert to standard notation
+			head /= 10
+			exp += 1		#every time we move the period, exp goes up 1
+
 		tail = (self.tail + other.tail) % TAIL_MAX
-		head = self.head + other.head
-		return HugeNumber(head, scientific_notation, tail)
+
+		return HugeNumber(head, exp, tail)
 
 	def is_squarefree(self):
 		for i in SQUARES:
@@ -55,20 +39,22 @@ class HugeNumber:
 		return True
 
 	def __str__(self):
-		return "{0:.1e} ... {1}".format(self.head, self.tail)
+		return "{0}e{1} ... {2}".format(self.head, self.exp, self.tail)
 
-def main():
-	prev = HugeNumber(1.0, ScientificNotation(100000, 0), 1)
-	cur = HugeNumber(1.0, ScientificNotation(100000, 0), 1)
+def main(count):
+	prev = HugeNumber(1.0, 0, 1)
+	cur = HugeNumber(1.0, 0, 1)
 
-	i = 2
-	while i < 200:
-		next = prev.add(cur)
+	print("1: {0}".format(prev))
+	print("2: {0}".format(cur))
+	i = 3
+	while i < count:
+		next = cur.add(prev)
 		prev = cur
 		cur = next
-		print(cur)
-		if cur.is_squarefree():
-			i += 1
-	print(cur)
+		#if cur.is_squarefree():
+		print("{0}: {1}".format(i, cur))
+		i += 1
+	print("{0}: {1}".format(i, cur))
 
-main()
+main(2000)
