5fc85b43789e80ac4d3aaa3d3a3ca915050f5bfd
petervel <peter.vel@nedap.com>
2012-10-29 18:16:38 +0100

    solved problem 40

--- problems_026-050/problem40.py
+++ problems_026-050/problem40.py
@@ -3,15 +3,17 @@ __author__ = 'peter.vel'
 
 def generate_string(length):
 	string = ""
-	i = 1
-	while len(string) < length:
+	i = 0
+	while len(string) < length + 1:
 		string += str(i)
 		i += 1
 	return string
 
 def main():
 	string = generate_string(1000000)
-	solution = int(string[1]) * int(string[10]) * int(string[100]) * int(string[1000]) * int(string[10000]) * int(string[100000]) * int(string[1000000])
+	solution = 1
+	for i in range(7):
+		solution *= int(string[10**i])
 	print(solution)
 
 main()
